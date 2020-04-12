927713146337525016405682ec0d9f7cd8768fda
lyuyu <liuyu9662@gmail.com>
2020-04-11 22:07:19 -0400

    "cardboard sdk imported, sample app with one sample picture"

---
+++ fantasee/src/main/java/pkg/myapp/MainActivity.java
@@ -0,0 +1,214 @@
+package pkg.myapp;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import com.google.vr.sdk.widgets.pano.VrPanoramaView;
+
+import java.io.InputStream;
+
+
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+    static {
+        System.loadLibrary("cardboard_jni");
+    }
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    // Permission request codes
+    private static final int PERMISSIONS_REQUEST_CODE = 2;
+
+    // Opaque native pointer to the native CardboardApp instance.
+    // This object is owned by the VrActivity instance and passed to the native methods.
+    private long nativeApp;
+
+    // Panorama photo viewer
+    private VrPanoramaView mVrPanoramaView;
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        nativeApp = nativeOnCreate(getAssets());
+
+        setContentView(R.layout.activity_vr);
+
+        mVrPanoramaView = (VrPanoramaView) findViewById(R.id.vrPanoramaView) ;
+        loadPhotoSphere();
+
+        // button.
+        setImmersiveSticky();
+        View decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener(
+                (visibility) -> {
+                    if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                        setImmersiveSticky();
+                    }
+                });
+
+        // Forces screen to max brightness.
+        WindowManager.LayoutParams layout = getWindow().getAttributes();
+        layout.screenBrightness = 1.f;
+        getWindow().setAttributes(layout);
+
+        // Prevents screen from dimming/locking.
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        nativeOnPause(nativeApp);
+        mVrPanoramaView.pauseRendering();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Checks for activity permissions, if not granted, requests them.
+        if (!arePermissionsEnabled()) {
+            requestPermissions();
+            return;
+        }
+        mVrPanoramaView.resumeRendering();
+        nativeOnResume(nativeApp);
+    }
+
+    @Override
+    protected void onDestroy() {
+        mVrPanoramaView.shutdown();
+        super.onDestroy();
+        nativeOnDestroy(nativeApp);
+        nativeApp = 0;
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            setImmersiveSticky();
+        }
+    }
+
+    /** Callback for when close button is pressed. */
+    public void closeSample(View view) {
+        Log.d(TAG, "Leaving VR mode");
+        finish();
+    }
+
+    /** Callback for when settings_menu button is pressed. */
+    public void showSettings(View view) {
+        PopupMenu popup = new PopupMenu(this, view);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.settings_menu, popup.getMenu());
+        popup.setOnMenuItemClickListener(this);
+        popup.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        if (menuItem.getItemId() == R.id.switch_viewer) {
+            nativeSwitchViewer(nativeApp);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Checks for activity permissions.
+     *
+     * @return whether the permissions are already granted.
+     */
+    private boolean arePermissionsEnabled() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    /** Handles the requests for activity permissions. */
+    private void requestPermissions() {
+        final String[] permissions = new String[] {Manifest.permission.READ_EXTERNAL_STORAGE};
+        ActivityCompat.requestPermissions(this, permissions, PERMISSIONS_REQUEST_CODE);
+    }
+
+    /** Callback for the result from requesting permissions. */
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (!arePermissionsEnabled()) {
+            Toast.makeText(this, R.string.no_permissions, Toast.LENGTH_LONG).show();
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(
+                    this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                // Permission denied with checking "Do not ask again".
+                launchPermissionsSettings();
+            }
+            finish();
+        }
+    }
+    private void launchPermissionsSettings() {
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.setData(Uri.fromParts("package", getPackageName(), null));
+        startActivity(intent);
+    }
+
+    private void setImmersiveSticky() {
+        getWindow()
+                .getDecorView()
+                .setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+
+    private void loadPhotoSphere() {
+        VrPanoramaView.Options options = new VrPanoramaView.Options();
+        InputStream inputStream = null;
+
+        AssetManager assetManager = getAssets();
+        try {
+            inputStream = assetManager.open("CQC.jpg");
+            options.inputType = VrPanoramaView.Options.TYPE_MONO;
+            mVrPanoramaView.loadImageFromBitmap(BitmapFactory.decodeStream(inputStream), options);
+            inputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {}
+
+    private native long nativeOnCreate(AssetManager assetManager);
+
+    private native void nativeOnDestroy(long nativeApp);
+
+    private native void nativeOnPause(long nativeApp);
+
+    private native void nativeOnResume(long nativeApp);
+
+    private native void nativeSwitchViewer(long nativeApp);
+}
